451bc9652b80bfb0d33084487cd86b72563fcd28
VamsiKadimisetti <119720281+VamsiKadimisetti@users.noreply.github.com>
2022-12-10 11:37:12 +0530

    #include<stdio.h> int main() {     char ch;     scanf("%c",&ch);     switch(ch)     {         case'V':printf("Violet");         break;         case'I':printf("Indigo");         break;         case'G':printf("Green");         break;         case'Y':printf("Yellow");         break;         case'O':printf("Orange");         break;         case'R':printf("Red");         break;         default:printf("-1");         break;     } }

---
+++ Print_the_color_name.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{ 
+    char ch; 
+    scanf("%c",&ch); 
+    switch(ch) 
+    { 
+        case'V':printf("Violet"); 
+        break; 
+        case'I':printf("Indigo");
+        break; 
+        case'G':printf("Green"); 
+        break; 
+        case'Y':printf("Yellow"); 
+        break;
+        case'O':printf("Orange"); 
+        break;
+        case'R':printf("Red"); 
+        break; 
+        default:printf("-1");
+        break; 
+    }
+}
