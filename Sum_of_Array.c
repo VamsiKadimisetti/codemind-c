f963ef302e424b351c10b5d5d008fd4fdb8149c7
VamsiKadimisetti <119720281+VamsiKadimisetti@users.noreply.github.com>
2022-12-10 12:13:23 +0530

    #include<stdio.h> int main() {     int n,sum=0;     scanf("%d",&n);     int arr[n],i;     for(i=0;i<n;i++)     scanf("%d",&arr[i]);     for(i=0;i<n;i++)     sum=sum+arr[i];     printf("%d",sum); }

---
+++ Sum_of_Array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0;
+    scanf("%d",&n);
+    int arr[n],i;
+    for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    for(i=0;i<n;i++)
+    sum=sum+arr[i];
+    printf("%d",sum);
+}
